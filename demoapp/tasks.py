1adf4c4091daa919d13d4c423b1e9239b1f6c3e7
Pogossian <gor@poghosyan.am>
2019-11-14 14:53:22 +0400

    remove demoapp

--- demoapp/tasks.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from celery import shared_task
-
-@shared_task
-def display_time():
-    print("The time is %s :" % str(datetime.now()))
-    return True
--- project_name/celery.py
+++ project_name/celery.py
@@ -7,14 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_name.settings')
 app = Celery('project_name')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    'display_time-20-seconds': {
-        'task': 'demoapp.tasks.display_time',
-        'schedule': 20.0
-    },
-}
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
